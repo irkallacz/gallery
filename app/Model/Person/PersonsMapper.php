554940c20538503170d03278eee4989442b463cd
Jakub Mottl <mottl@vzs-jablonec.cz>
2022-09-28 12:03:38 +0200

    Update person mapper for rights

--- app/Model/Person/PersonsMapper.php
+++ app/Model/Person/PersonsMapper.php
@@ -4,9 +4,20 @@
 namespace App\Model\Person;
 
 
+use App\Model\Right\RightsMapper;
+use Nextras\Orm\Entity\Reflection\PropertyMetadata;
+use Nextras\Orm\Mapper\IMapper;
 use Nextras\Orm\Mapper\Mapper;
 
 final class PersonsMapper extends Mapper
 {
+	protected $tableName = 'user';
 
+	public function getManyHasManyParameters(PropertyMetadata $sourceProperty, IMapper $targetMapper): array
+	{
+		if ($targetMapper instanceof RightsMapper) {
+			return ['user_rights', ['user_id', 'rights_id']];
+		}
+		return parent::getManyHasManyParameters($sourceProperty, $targetMapper);
+	}
 }
