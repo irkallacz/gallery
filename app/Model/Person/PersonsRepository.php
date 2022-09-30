347c3502f6a5683d36017b44a62896f910ee001a
Jakub Mottl <mottl@vzs-jablonec.cz>
2022-09-30 11:42:07 +0200

    Update person repository

--- app/Model/Person/PersonsRepository.php
+++ app/Model/Person/PersonsRepository.php
@@ -14,9 +14,9 @@ final class PersonsRepository extends Repository
 		return [Person::class];
 	}
 
-	public function findByRole(int $role = Person::ROLE_MEMBER): ICollection|array
+	public function findByRole(int $role = Person::ROLE_MEMBER): ICollection | array
 	{
-		return $this->findBy(['role>=' => $role]);
+		return $this->findBy(['role>' => $role]);
 	}
 
 	public function getPersonList(int $role = Person::ROLE_USER): array
@@ -24,26 +24,5 @@ final class PersonsRepository extends Repository
 		return $this->findByRole($role)
 			->orderBy(['surname' => ICollection::ASC, 'name' => ICollection::ASC])
 			->fetchPairs('id', 'fullName');
-	}
-
-	public function getByMail(string $mail): ?Person
-	{
-		return $this->getBy([ICollection::OR, 'mail' => $mail, 'mail2' => $mail]);
-	}
-
-	public function getByCredentials(string $name, string $surname, \DateTimeImmutable $dateBorn): ?Person
-	{
-		return $this->getBy(['name' => $name, 'surname' => $surname, 'dateBorn' => $dateBorn]);
-	}
-
-	public function isEmailUnique(string $mail, ?int $person): bool
-	{
-		$conditions = [ICollection::OR, 'mail' => $mail, 'mail2' => $mail];
-
-		if ($person) {
-			$conditions = [ICollection::AND, ['id!=' => $person], $conditions];
-		}
-
-		return !(bool) $this->getBy($conditions);
 	}
 }
